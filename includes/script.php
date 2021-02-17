e8a10ea6f3d1874815e0850b698d8ba3a3e98d5b
Brian Therens <brian.therens@gmail.com>
2021-02-17 17:14:20 -0600

    * organized js functions into collections that make sense together * many functions are unneeded for this project, but am leaving here for now because they work well within my MVC framework and could prove useful to present modals or handle form submissions

--- includes/script.php
+++ includes/script.php
@@ -1,3 +1,8 @@
 <script language='javascript' type='text/javascript'>
-    <?php include 'js/base.js'; ?>
+    <?php
+        include 'js/base.js';
+        include 'js/ajax.js';
+        include 'js/modal.js';
+        include 'js/active.js';
+    ?>
 </script>
