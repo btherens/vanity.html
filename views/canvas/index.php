54575b8c5d162d04d8da72674a2ab171b782f2f3
Brian Therens <brian.therens@gmail.com>
2021-02-17 17:37:58 -0600

    * complete svg refactor

--- views/canvas/index.php
+++ views/canvas/index.php
@@ -16,35 +16,13 @@
 
                         <?php echo $profile ?>
                         <hr>
-                        <h3>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-180.000000, -2559.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M134,2411 C132.897,2411 132,2410.103 132,2409 C132,2407.897 132.897,2407 134,2407 C135.103,2407 136,2407.897 136,2409 C136,2410.103 135.103,2411 134,2411 L134,2411 Z M134,2405 C131.791,2405 130,2406.791 130,2409 C130,2411.209 131.791,2413 134,2413 C136.209,2413 138,2411.209 138,2409 C138,2406.791 136.209,2405 134,2405 L134,2405 Z M141.254,2412.043 C139.46,2412.777 139.453,2412.78 138.339,2413.236 C137.915,2414.246 138.259,2413.425 137.019,2416.382 C134.43,2415.296 135.05,2415.556 133.872,2415.062 C132.868,2415.473 133.531,2415.202 130.957,2416.254 C130.223,2414.46 130.22,2414.453 129.764,2413.339 C128.755,2412.915 129.575,2413.259 126.618,2412.019 C127.704,2409.43 127.444,2410.05 127.938,2408.872 C127.733,2408.372 127.782,2408.491 126.746,2405.957 C129.308,2404.909 128.608,2405.195 129.661,2404.764 C130.081,2403.764 129.743,2404.57 130.981,2401.618 C133.57,2402.704 132.95,2402.444 134.128,2402.938 C134.628,2402.733 134.509,2402.782 137.043,2401.746 C138.088,2404.301 138.031,2404.161 138.236,2404.661 C139.245,2405.085 138.425,2404.741 141.382,2405.981 C140.202,2408.794 140.517,2408.044 140.062,2409.128 C140.267,2409.628 140.218,2409.509 141.254,2412.043 L141.254,2412.043 Z M144,2404.911 L139.773,2403.138 L138.138,2399.138 L134.138,2400.773 L129.911,2399 L128.138,2403.227 L124.138,2404.862 L125.773,2408.862 L124,2413.089 L128.227,2414.862 L129.862,2418.862 L133.862,2417.227 L138.089,2419 L139.862,2414.773 L143.862,2413.138 L142.227,2409.138 L144,2404.911 Z" id="star-[#1259]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>skills</h3>
+                        <h3><div class="icon baseline"><?php InlineAsset::svg( 'gear' ) ?></div>skills</h3>
                         <p>Adobe Photoshop</p>
                         <p>Photography</p>
                         <p>Illustrator</p>
                         <p>Media</p>
                         <hr>
-                        <h3>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1"fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2399.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M336.311,2256.66477 C337.577,2254.61206 338.313,2252.32917 338.507,2250.00323 L341.93,2250.00323 C341.533,2253.16786 339.289,2255.76302 336.311,2256.66477 M334.005,2256.50564 C332.565,2254.55702 331.719,2252.30815 331.495,2250.00323 L336.506,2250.00323 C336.285,2252.30715 335.442,2254.55602 334.005,2256.50564 M326.069,2250.00323 L329.493,2250.00323 C329.689,2252.33017 330.429,2254.61507 331.699,2256.66777 C328.716,2255.76903 326.467,2253.17187 326.069,2250.00323 M331.288,2241.47915 C330.176,2243.51284 329.574,2245.7457 329.467,2248.00157 L326.069,2248.00157 C326.448,2244.98106 328.512,2242.48499 331.288,2241.47915 M333.967,2240.99675 C333.976,2240.98474 333.985,2240.97273 333.994,2240.96072 C334.003,2240.97273 334.011,2240.98474 334.02,2240.99675 C335.555,2243.08749 336.393,2245.52551 336.532,2248.00157 L331.467,2248.00157 C331.603,2245.52651 332.437,2243.08949 333.967,2240.99675 M341.93,2248.00157 L338.532,2248.00157 C338.422,2245.74369 337.816,2243.50984 336.699,2241.47415 C339.482,2242.47798 341.551,2244.97606 341.93,2248.00157 M334.855,2239.03712 C328.942,2238.5347 324,2243.19858 324,2249.0024 C324,2254.43892 328.335,2258.84258 333.71,2258.99571 C339.387,2259.15984 344,2254.59505 344,2249.0024 C344,2243.84011 340.068,2239.48249 334.855,2239.03712" id="globe_network-[#1294]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>languages</h3>
+                        <h3><div class="icon baseline"><?php InlineAsset::svg( 'gear' ) ?></div>languages</h3>
                         <p>English</p>
                         <p>Spanish</p>
                         <p>German</p>
@@ -55,33 +33,10 @@
             <div class="col-2">
 
                 <div class="container card">
-                    <h2>
-                        <div class="icon baseline">
-                            <svg  viewBox="0 0 23 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                    <g id="Dribbble-Light-Preview" transform="translate(-337.000000, -1559.000000)">
-                                        <g id="icons" transform="translate(56.000000, 160.000000)">
-                                            <path d="M297.8606,1399.0005 L283.9996,1399.0005 L283.9996,1412.97382 L285.9996,1412.97382 L285.9996,1400.99669 L295.9996,1400.99669 L295.9996,1406.98526 L301.9996,1406.98526 L301.9996,1416.9662 L289.9996,1416.9662 L289.9996,1418.96239 L303.9996,1418.96239 L303.9996,1405.06991 L297.8606,1399.0005 Z M287.2916,1414.69953 L288.7066,1416.11083 L283.8076,1421.0005 L283.7216,1420.91466 L283.7216,1420.91466 L280.9996,1418.19785 L282.4146,1416.78654 L283.8076,1418.17689 L287.2916,1414.69953 Z" id="file_done-[#1684]"></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>work experience</h2>
+                    <h2><div class="icon baseline"><?php InlineAsset::svg( 'filedone' ) ?></div>work experience</h2>
                     <div>
                         <h5>Front End Developer / altavista.com</h5>
-                        <h6>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2799.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M326,2657 L342,2657 L342,2645 L326,2645 L326,2657 Z M342,2641 L342,2639 L340,2639 L340,2641 L335,2641 L335,2639 L333,2639 L333,2641 L328,2641 L328,2639 L326,2639 L326,2641 L324,2641 L324,2659 L344,2659 L344,2641 L342,2641 Z" id="calendar-[#1194]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>Jan 2015 - <span
-                                class="highlight">Current</span></h6>
+                        <h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div>Jan 2015 - <span class="highlight">Current</span></h6>
                         <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est
                             reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure,
                             iste.</p>
@@ -89,18 +44,7 @@
                     </div>
                     <div>
                         <h5>Web Developer / something.com</h5>
-                        <h6>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2799.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M326,2657 L342,2657 L342,2645 L326,2645 L326,2657 Z M342,2641 L342,2639 L340,2639 L340,2641 L335,2641 L335,2639 L333,2639 L333,2641 L328,2641 L328,2639 L326,2639 L326,2641 L324,2641 L324,2659 L344,2659 L344,2641 L342,2641 Z" id="calendar-[#1194]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>Mar 2012 - Dec 2014</h6>
+                        <h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div>Mar 2012 - Dec 2014</h6>
                         <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est
                             reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure,
                             iste.</p>
@@ -108,66 +52,22 @@
                     </div>
                     <div>
                         <h5>Graphic Designer / designsomething.com</h5>
-                        <h6>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2799.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M326,2657 L342,2657 L342,2645 L326,2645 L326,2657 Z M342,2641 L342,2639 L340,2639 L340,2641 L335,2641 L335,2639 L333,2639 L333,2641 L328,2641 L328,2639 L326,2639 L326,2641 L324,2641 L324,2659 L344,2659 L344,2641 L342,2641 Z" id="calendar-[#1194]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>Jun 2010 - Mar 2012</h6>
+                        <h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div>Jun 2010 - Mar 2012</h6>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                     </div>
                 </div>
 
                 <div class="container card">
-                <h2>
-                        <div class="icon baseline">
-                            <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                    <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -1399.000000)">
-                                        <g id="icons" transform="translate(56.000000, 160.000000)">
-                                            <path d="M297.9792,1239.0005 L284.0002,1239.0005 L284.0002,1249.0005 L286.0002,1249.0005 L286.0002,1241.0005 L296.0002,1241.0005 L296.0002,1247.0005 L302.0002,1247.0005 L302.0002,1249.0005 L304.0002,1249.0005 L304.0002,1245.4385 L297.9792,1239.0005 Z M295.0002,1254.1055 C295.0002,1253.5525 294.5522,1253.1055 294.0002,1253.1055 L293.0002,1253.1055 L293.0002,1255.1055 L294.0002,1255.1055 C294.5522,1255.1055 295.0002,1254.6575 295.0002,1254.1055 L295.0002,1254.1055 Z M295.7242,1256.4755 L297.0002,1259.0005 L295.0002,1259.0005 L294.0002,1257.0005 L293.0002,1257.0005 L293.0002,1259.0005 L291.0002,1259.0005 L291.0002,1251.0005 L294.0002,1251.0005 C295.6572,1251.0005 297.0002,1252.3965 297.0002,1254.0525 C297.0002,1255.0665 296.4942,1255.9325 295.7242,1256.4755 L295.7242,1256.4755 Z M286.0002,1251.0005 C284.8952,1251.0005 284.0002,1252.0005 284.0002,1253.1055 L284.0002,1254.1055 C284.0002,1255.2095 284.8952,1256.0005 286.0002,1256.0005 L287.5002,1256.0005 C287.7762,1256.0005 288.0002,1256.2245 288.0002,1256.5005 C288.0002,1256.7765 287.7762,1257.0005 287.5002,1257.0005 L284.0002,1257.0005 L284.0002,1259.0005 L288.0002,1259.0005 L288.0212,1259.0005 C289.1142,1259.0005 290.0002,1258.2195 290.0002,1257.1255 L290.0002,1256.0845 C290.0002,1254.9915 289.1142,1254.0005 288.0212,1254.0005 L288.0002,1254.0005 L286.5002,1254.0005 C286.2242,1254.0005 286.0002,1253.7765 286.0002,1253.5005 C286.0002,1253.2245 286.2242,1253.0005 286.5002,1253.0005 L290.0002,1253.0005 L290.0002,1251.0005 L286.0002,1251.0005 Z M302.0002,1251.0005 L304.0002,1251.0005 L304.0002,1253.0005 L302.0002,1253.0005 C300.8952,1253.0005 300.0002,1253.8955 300.0002,1255.0005 C300.0002,1256.1045 300.8952,1257.0005 302.0002,1257.0005 L304.0002,1257.0005 L304.0002,1259.0005 L302.0002,1259.0005 C299.7912,1259.0005 298.0002,1257.2095 298.0002,1255.0005 C298.0002,1252.7915 299.7912,1251.0005 302.0002,1251.0005 L302.0002,1251.0005 Z" id="file_src-[#1724]"></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>education</h2>
+                    <h2><div class="icon baseline"><?php InlineAsset::svg( 'filesource' ) ?></div>education</h2>
                     <div>
                         <h5>Yale Law School</h5>
-                        <h6>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2799.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M326,2657 L342,2657 L342,2645 L326,2645 L326,2657 Z M342,2641 L342,2639 L340,2639 L340,2641 L335,2641 L335,2639 L333,2639 L333,2641 L328,2641 L328,2639 L326,2639 L326,2641 L324,2641 L324,2659 L344,2659 L344,2641 L342,2641 Z" id="calendar-[#1194]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>2010 - 2014</h6>
+                        <h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div>2010 - 2014</h6>
                         <p>Web Development! All I need to know in one place</p>
                         <hr>
                     </div>
                     <div>
                         <h5>London Business School</h5>
-                        <h6>
-                            <div class="icon baseline">
-                                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-380.000000, -2799.000000)">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <path d="M326,2657 L342,2657 L342,2645 L326,2645 L326,2657 Z M342,2641 L342,2639 L340,2639 L340,2641 L335,2641 L335,2639 L333,2639 L333,2641 L328,2641 L328,2639 L326,2639 L326,2641 L324,2641 L324,2659 L344,2659 L344,2641 L342,2641 Z" id="calendar-[#1194]"></path>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>2006 - 2010</h6>
+                        <h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div>2006 - 2010</h6>
                         <p>Master Degree</p>
                     </div>
                 </div>
