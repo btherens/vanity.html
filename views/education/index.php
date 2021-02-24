3395213ad8a66f8a896e3ac0c3c191af8000f008
Brian Therens <brian.therens@gmail.com>
2021-02-23 18:57:07 -0600

    * list styles added * list added to education model and education view * section breaks in work and education views moved into a more fitting structure

--- views/education/index.php
+++ views/education/index.php
@@ -4,10 +4,11 @@
         <?php foreach ( $educations as $i => $education ) { ?>
             <div>
                 <h5><?php echo $education->subject ?> / <?php echo $education->organization ?></h5>
-                <?php if ( $education->startdate   )       { ?><h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div><?php echo date( 'F Y', strtotime( $education->startdate ) ) ?> – <?php if ( $education->enddate ) { echo date( 'F Y', strtotime( $education->enddate ) ); } else { ?><span class="highlight">Current</span><?php } ?></h6><?php } ?>
-                <?php if ( $education->description )       { ?><p><?php echo $education->description ?></p><?php } ?>
-                <?php if ( $i < count( $educations ) - 1 ) { ?><hr><?php } else { ?><br><?php } ?>
+                <?php if ( $education->startdate   ) { ?><h6><div class="icon baseline"><?php InlineAsset::svg( 'calendar' ) ?></div><?php echo date( 'F Y', strtotime( $education->startdate ) ) ?> – <?php if ( $education->enddate ) { echo date( 'F Y', strtotime( $education->enddate ) ); } else { ?><span class="highlight">Current</span><?php } ?></h6><?php } ?>
+                <?php if ( $education->description ) { ?><p><?php echo $education->description ?></p><?php } ?>
+                <?php if ( $education->list )        { ?><ul role="list"><?php foreach ( $education->list as $li ) { ?><li><?php echo $li ?></li><?php } ?></ul><?php } ?>
             </div>
+            <div><?php if ( $i < count( $educations ) - 1 ) { ?><hr><?php } else { ?><br><?php } ?></div>
         <?php } ?>
     </div>
 <?php } ?>
