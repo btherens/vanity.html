b77f5a49f61a732bee3d80d4595a73f17c5e9bba
Brian Therens <brian.therens@gmail.com>
2021-03-02 19:17:02 -0600

    * bad use of divs inside p tags * styles updated to maintain visual look

--- views/method/index.php
+++ views/method/index.php
@@ -2,7 +2,7 @@
     <hr>
     <h3><div class="icon baseline"><?php InlineAsset::svg( 'globe' ) ?></div>methods</h3>
     <?php foreach ( $methods as $method ) { ?>
-        <div class="labelbar" >
+        <div class="panelrow labelbar" >
             <div class="label"><?php echo $method->label ?></div>
             <?php if ( $method->aptitude ) { ?><div class="progressbar" ><div style="width:<?php echo $method->aptitude * 100 ?>%"></div></div><?php } ?>
         </div>
