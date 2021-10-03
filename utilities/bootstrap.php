73be7bfd8307041d016c85f31d736452d5850879
Brian Therens <brian.therens@gmail.com>
2021-10-03 09:19:41 -0500

    * base view class cleanup * small correction to comment in bootstrap

--- utilities/bootstrap.php
+++ utilities/bootstrap.php
@@ -35,7 +35,7 @@ if   ( class_exists( $controller ) ) { $load = new $controller( $action ); }
 /* throw an error if it doesn't */
 else { http_response_code( 404 ); die( 'please check your url' ); }
 
-/* execute method and send to output buffer */
+/* execute method and send result to output */
 if   ( method_exists( $load, $action ) ) { echo $load->$action( $query ); }
 /* throw an error if we couldn't find a method that matched request */
 else { http_response_code( 404 ); die( 'Invalid method. Please check the URL.' ); }
