aa50ede4b678b7b027298dd4eff3812a498a4289
Brian Therens <brian.therens@gmail.com>
2022-01-22 11:50:59 -0600

    * improved css for print media * clean up css and add comments * minor cleanup / reorganization to views - each view has its own open / close tags now * wkhtmltopdf command cleaned up - styles like zoom are now properly handled in css

--- includes/style.php
+++ includes/style.php
@@ -2,26 +2,33 @@
     <?php
         /* detect internal rendering requests to serve alternate styles */
         $printmode = (bool) ISSET( $_SERVER[ 'HTTP_USER_AGENT' ] ) ? ( $_SERVER[ 'HTTP_USER_AGENT' ] == 'internal-pdf-render' ) : false;
+        $printmode = true;
+        /* evaluate CSS variables for print server to support older browsing engine */
         function evalCssVars( string $buffer ): string
         {
             $matches = [];
             /* get variable definitions */
             $cssVars = preg_match_all( '/^\s*(--\S+):\s*(.+);\s*$/m', $buffer, $matches );
-            // (?<=:\s)var\(--canvas-accentcolor1\)(?=\s*;)
+            /* convert key into regex match pattern */
             function evalPregPat( &$str ) { $str = '/(?<=:\s)var\(' . preg_quote( $str ) . '\)(?=\s*;)/'; }
+            /* step through each record in array and convert to regex pattern using evalPregPat */
             array_walk( $matches[ 1 ], 'evalPregPat' );
 
+            /* replace each var() with the css variable's true value */
             $newbuffer = preg_replace( $matches[1], $matches[2], $buffer );
+            /* return buffer after alteration is complete */
             return $newbuffer;
         }
+        /* catch included css files in buffer and postprocess with evalCssVars */
         if ( $printmode ) { ob_start( 'evalCssVars' ); }
-        /* embed style files directly */
+        /* embed style files */
         include 'css/variable.css';
         include 'css/responsive.css';
         include 'css/font.css';
         include 'css/element.css';
         include 'css/layout.css';
         include 'css/icon.css';
+        /* run included css through evalCssVars for print styles */
         if ( $printmode ) { ob_end_flush(); }
     ?>
 </style>
