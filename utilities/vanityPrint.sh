ce38e6dfd10857c46f26ed01301362d56022698c
Brian Therens <brian.therens@gmail.com>
2022-01-24 17:23:12 -0600

    vanityPrint.sh refactor
    * usage() presents function info, called if inputs aren't valid
    * functions given stdIn inputs to allow piping from function to function (instead of string evaluation)
    * trimPdfCache split from getPdfCache and called if -w flag passed in command
    * stderr stream used for exceptions

--- utilities/vanityPrint.sh
+++ utilities/vanityPrint.sh
@@ -1,7 +1,27 @@
-#!/bin/bash
-# vanityPrint - returns a filepath to a rendered pdf document
-# e.g. bash vanityPrint.sh -u "www.google.com" -d "resource/pdf" -t "Title" -s "Subject" -c "Creator";
-while getopts u:d:t:s:c:a: option
+#!/usr/bin/env bash
+usage() { cat << EOF
+usage: vanityPrint -u URL -d DIR -t TITLE -s SUBJECT -c CREATOR -a AGENT [-w]
+
+renders a pdf of the given url and saves the result to DIR. DIR serves as this PDF's cache.
+
+returns the full filepath to a rendered pdf document
+
+OPTIONS:
+    -u URL     internet address to render e.g. http://google.com
+    -d DIR     directory to save results to (will be used as a file cache)
+
+    PDF PROPERTIES
+    -t TITLE   document's title
+    -s SUBJECT document's subject
+    -c CREATOR creator of the document - default is 'vanity.html'
+
+    -a AGENT   pass a custom user agent to use in web request - default is 'internal-pdf-render'
+    -w TRIM    pass -w flag to drop files older than timeout (10 minutes)
+
+EOF
+}
+
+while getopts :u:d:t:s:c:a:w option
 do
     case "${option}"
     in
@@ -18,6 +38,10 @@ do
         c) CREATOR=${OPTARG};;
         # custom user agent to use in http request ( DEFAULT 'internal-pdf-render' )
         a) AGENT=${OPTARG};;
+        # pass -w to drop files older than timeout
+        w) TRIM=1;;
+        # return usage info if any unsupported parameters were passed
+        ?) usage; exit 1;;
     esac
 done
 # define defaults
@@ -25,32 +49,35 @@ done
 [ -z "$CREATOR" ] && CREATOR='vanity.html';
 
 # test for binaries and fail critically if we need to
-for i in 'wkhtmltopdf' 'exiftool'; do command -v $i >/dev/null 2>&1 || { echo "$i required but not found! failing critically..."; exit 1; }; done;
+for i in 'wkhtmltopdf' 'exiftool'; do command -v $i &>/dev/null || { echo "$i required but not found! failing critically..." 1>&2; exit 1; }; done;
 
 # renderPdf - render a url and save a randomly named file to the given directory
 function renderPdf() {
-    # url to render
-    local URL=$1;
+    # StdIn: url to render
+    local URL; read URL;
     # output filename
-    local PDF=$2;
+    local PDF=$1;
     # user agent to use in request
-    local AGENT=$3;
-    # delete destination if it exists
-    rm -f $2 &>/dev/null;
+    local AGENT=$2;
+
+    # remove any existing file at PDF path
+    rm -f "$PDF" &>/dev/null;
+
     # render pdf and fail critically upon exception
-    wkhtmltopdf -T 0 -L 0 -R 0 -B 0 --print-media-type --custom-header 'User-Agent' "$AGENT" --custom-header-propagation $URL $PDF &>/dev/null || { echo 'error: wkhtml exceptions!'; return 1; };
+    wkhtmltopdf -T 0 -L 0 -R 0 -B 0 --print-media-type --custom-header 'User-Agent' "$AGENT" --custom-header-propagation $URL $PDF &>/dev/null || { echo 'error: wkhtml exceptions!' 1>&2; return 1; };
     # return saved filepath and exit
     echo $PDF && return 0;
 }
 
 # setPdfProp - update a PDF's properties and save in place
 function setPdfProp() {
-    # the PDF to set properties on
-    local PDF=$1;
-    # the properties to apply
-    local TITLE=$2;
-    local SUBJECT=$3;
-    local CREATOR=$4;
+    # StdIn: the PDF to set properties on
+    local PDF; read PDF;
+    # args: the properties to apply
+    local TITLE=$1;
+    local SUBJECT=$2;
+    local CREATOR=$3;
+
     # execute pdf property update
     exiftool \
         -z -P \
@@ -61,7 +88,7 @@ function setPdfProp() {
         -PDF:Creator="$CREATOR" -XMP:CreatorTool="$CREATOR" \
         -Producer="$CREATOR" \
         -overwrite_original_in_place "$PDF" \
-    &>/dev/null || { echo 'error: exiftool exceptions!'; return 1; };
+    &>/dev/null || { echo 'error: exiftool exceptions!' 1>&2; return 1; }
     # return path and exit
     echo $PDF && return 0;
 }
@@ -69,42 +96,63 @@ function setPdfProp() {
 # save an input pdf to cache path with a random name
 function setPdfCache() {
     # the finished file we want to move
-    local INPUT=$1;
+    local INPUT; read INPUT;
     # generate filename
-    local OUTPUT="$2/$( uuidgen ).pdf";
+    local OUTPUT="$1/$( uuidgen ).pdf";
     # move input file to final output path and pass result to stdOut
-    mv -f "$INPUT" "$OUTPUT" &>/dev/null || { echo 'error: could not save result'; return 1; }
+    mv -f "$INPUT" "$OUTPUT" &>/dev/null || { echo 'error: could not save result' 1>&2; return 1; }
     # return result and exit
     echo $OUTPUT && return 0;
 }
 
 # attempt to get a pdf from given cache location
 function getPdfCache() {
-    # the directory to use as pdf cache
-    local DIR=$1;
+    # Stdin: the directory to use as pdf cache
+    local DIR; read DIR;
+    # arg1: override default timeout in minutes (1 minute)
+    local TIMEOUT=$1; [ -z "$TIMEOUT" ] && TIMEOUT='1';
     # create the directory if it does not exist
     mkdir -p $DIR &>/dev/null;
-    # drop files older than 10 minutes
-    find $DIR/*.pdf -type f -mmin +10 -maxdepth 1 2>/dev/null | xargs rm;
     # get an existing pdf created in the last 1 minute
-    find $DIR/*.pdf -type f -mmin -1 -maxdepth 1 2>/dev/null | grep -m1 .;
-    # exit function
-    return 0;
+    find $DIR/*.pdf -type f -mmin "-${TIMEOUT}" -maxdepth 1 2>/dev/null | grep -m1 .;
+    # exit function with exit code from find command
+    return $?;
 }
 
-# get an existing pdf from cache (returns nothing if no cache file was found)
-FILE=$( getPdfCache $DIR );
+# attempt to get a pdf from given cache location
+function trimPdfCache() {
+    # Stdin: the directory to use as pdf cache
+    local DIR; read DIR;
+    # arg1: override default timeout in minutes (10 minutes)
+    local TIMEOUT=$1; [ -z "$TIMEOUT" ] && TIMEOUT='10';
+    # create the directory if it does not exist
+    mkdir -p $DIR &>/dev/null;
+    # drop files older than timeout
+    find $DIR/*.pdf -type f -mmin "+${TIMEOUT}" -maxdepth 1 2>/dev/null | xargs rm;
+    # exit function with exit code from find command
+    return $?;
+}
 
-# continue with making new pdf if we didn't find a valid result in cache
-if [ -z "$FILE" ];
-then
-    # create temporary file
-    TMPPATH=$( mktemp );
-    # be sure to delete what remains when exiting
-    trap "rm -f $TMPPATH;" EXIT;
-    # create a pdf, set properties, and save to final path $FILE
-    FILE=$( setPdfCache $( setPdfProp "$( renderPdf $URL $TMPPATH $AGENT )" "$TITLE" "$SUBJECT" "$CREATOR" ) $DIR ) || { echo "errors encounters when creating new pdf!"; exit 1; }
-fi;
+# proceed if a directory was passed
+if [ -z "$DIR" ]; then usage; exit 1; else
 
-# return result and exit code
-[ -z "$FILE" ] && exit 1 || { echo $FILE && exit 0; };
+    # trim cache directory if switch was passed
+    [ "$TRIM" == '1' ] && echo "$DIR" | trimPdfCache;
+
+    # proceed if URL was passed
+    if [ -n "$URL" ]; then
+
+        # get an existing pdf from cache (returns nothing if no cache file was found)
+        FILE=$( echo "$DIR" | getPdfCache );
+
+        # create temporary path for pdf rendering and configure trap to ensure script cleans up temporary files on exit
+        TMPPATH=$( mktemp ); trap 'rm -f "'"$TMPPATH"'";' EXIT;
+
+        # create a pdf, set properties, and save to final path $FILE if we didn't get a result from cache
+        [ -z "$FILE" ] && { FILE="$( echo "$URL" | renderPdf "$TMPPATH" "$AGENT" | setPdfProp "$TITLE" "$SUBJECT" "$CREATOR" | setPdfCache "$DIR" )" || { echo 'errors encounters when creating new pdf!' 1>&2; exit 1; } }
+
+        # return result and exit code
+        [ -z "$FILE" ] && exit 1 || { echo "$FILE" && exit 0; };
+
+    fi
+fi
